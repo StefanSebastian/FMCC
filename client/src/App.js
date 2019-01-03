b5e5f2754adb8285639162bd6796615e761d1769
StefanSebastian <stefansebii@gmail.com>
2019-01-03 11:03:05 +0200

    Added basic routing support

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import ClientPage from './ClientPage';
+import AdminPage from './AdminPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello world
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/client'>Client menu</Link></li>
+            <li><Link to='/admin'>Admin menu</Link></li>
+          </ul>
+
+          <Route path='/client' component={ClientPage} />
+          <Route path='/admin' component={AdminPage} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/ClientPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClientPage extends Component {
+    render() {
+        return(
+            <div className="ClientPage">
+                Client page
+            </div>
+        );
+    }
+}
+
+export default ClientPage;
